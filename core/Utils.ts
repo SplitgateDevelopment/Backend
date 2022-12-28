3553d687d77d67a433c3927a8cb383994d9ec49b
PADO <62028267+47PADO47@users.noreply.github.com>
2022-12-28 14:00:48 +0100

    ✨ Add Utils#sendWS() and Utils#decodeWS()

--- core/Utils.ts
+++ core/Utils.ts
@@ -1,3 +1,6 @@
+import WebSocket from "ws";
+import { wsMessage } from "../typings/WS";
+
 class Utils {
     static randomInt(min: number, max: number): number {
         return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -7,8 +10,7 @@ class Utils {
         const upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
         const lower = 'abcdefghijklmnopqrstuvwxyz';
         const numbers = '0123456789';
-        const special = '!@#$%^&*()_+~`|}{[]\:;?><,./-=';
-        const chars = upper + lower + numbers + special;
+        const chars = upper + lower + numbers;
 
         let result = '';
         for (let i = 0; i < length; i++) {
@@ -19,6 +21,33 @@ class Utils {
 
     static randomArrayElement<T>(array: T[]): T {
         return array[Math.floor(Math.random() * array.length)];
+    };
+
+    static sendWS(ws: WebSocket, data: wsMessage) {
+        if (!(data instanceof Object)) throw new Error(`Data must be an object!`);
+
+        let message = "";
+        Object.keys(data)
+        .forEach(key => {
+            if (data[key] instanceof Object) data[key] = JSON.stringify(data[key]);
+            message += `${key}: ${data[key]}\n`;
+        });
+
+        ws.send(message);
+    };
+
+    static decodeWS(data: string): wsMessage {
+        const lines = data.split(`\n`);
+        const result: wsMessage = {
+            type: "",
+        };
+
+        lines.forEach(line => {
+            const [key, value] = line.split(`: `);
+            result[key] = value;
+        });
+
+        return result;
     };
 };
 
