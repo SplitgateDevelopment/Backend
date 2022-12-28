8a62c3fb1dedcd28ea354c0d58ac4e8d581ec102
PADO <62028267+47PADO47@users.noreply.github.com>
2022-12-28 14:14:59 +0100

    ✨ Create items route

---
+++ routes/items.ts
@@ -0,0 +1,23 @@
+import Route from "../core/Route";
+import { Request, Response, Router } from "express";
+import { item } from "../typings/Game";
+
+const router = Router();
+
+router.get(`/locale/byIds`, async (req: Request, res: Response) => {
+    const itemIds = req.query.itemIds?.toString().split(',');
+    const items: item[] = [];
+    
+    if (!itemIds) return res.status(200).json({});
+    for (const itemId of itemIds) {
+        const item: item = await import(`../assets/json/items/${itemId}.json`);
+        items.push(item);
+    };
+
+    return res.status(200).json(items);
+});
+
+export default new Route({
+    url: `/platform/public/namespaces/splitgate/items`,
+    router,
+})
