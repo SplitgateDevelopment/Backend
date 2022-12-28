a2255eaf03fc3da542e2909b8512ff1ebf6eff2a
PADO <62028267+47PADO47@users.noreply.github.com>
2022-12-28 14:14:08 +0100

    💸 Create views routes

---
+++ routes/views.ts
@@ -0,0 +1,50 @@
+import Route from "../core/Route";
+import { Request, Response, Router } from "express";
+import { viewInfo, viewItem } from "../typings/Game";
+
+const router = Router();
+
+router.get(`/`, async (req: Request, res: Response) => {
+    const { default: data } = await import('../assets/json/views.json') as { default: viewInfo[] };
+    const views = data.map((v) => {
+        const date = new Date();
+        date.setHours(date.getHours() - 2);
+        v['createdAt'] = date.toISOString();
+        v['updatedAt'] = date.toISOString();
+        return v;
+    });
+
+    return res.status(200).json(views);
+});
+
+router.get(`/:id/sections`, async (req: Request, res: Response) => {
+    const { default: data } = await import(`../assets/json/views/${req.params.id}.json`);
+    
+    const date = new Date();
+    date.setHours(date.getHours() - 2);
+
+    const view = {
+        ...data,
+        items: data.items.map((i: viewItem) => {
+            i['updatedAt'] = date.toISOString();
+            i['createdAt'] = date.toISOString();
+            return i;
+        }),
+        startDate: date.toISOString(),
+        endDate: date.toISOString(),
+        updatedAt: date.toISOString(),
+        createdAt: date.toISOString(),
+        viewId: req.params.id
+    }
+    return res.status(200).json([
+        {
+            viewId: req.params.id,
+            ...view
+        }
+    ]);
+});
+
+export default new Route({
+    url: `/platform/public/namespaces/splitgate/views`,
+    router,
+})
