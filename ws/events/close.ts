f40a123f79eb75d647a393725dd1406e23f98af9
PADO <62028267+47PADO47@users.noreply.github.com>
2022-12-28 14:04:24 +0100

    ♻️ Update open and close events

--- ws/events/close.ts
+++ ws/events/close.ts
@@ -1,9 +1,8 @@
-import { WebSocket } from "ws";
 import { eventHandler } from "../../typings/WS";
 
 export default {
     name: "close",
-    handler: (server, ws: WebSocket) => {
+    handler: () => {
         console.log(`[WS]`, `Connection closed 📶`);
     }
 } as eventHandler
--- ws/events/open.ts
+++ ws/events/open.ts
@@ -1,9 +1,8 @@
-import { WebSocket } from "ws";
 import { eventHandler } from "../../typings/WS";
 
 export default {
     name: "open",
-    handler: (server, ws: WebSocket) => {
+    handler: () => {
         console.log(`[WS]`, `Connection opened 📶`);
     }
 } as eventHandler
